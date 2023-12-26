c1ab392b0323a2fc0417821b27fd2e348789b1d6
zsaveleva <zsaveleva>
2023-12-26 18:05:27 +0300

    update

--- src/Test.tsx
+++ src/Test.tsx
@@ -4,7 +4,10 @@ export const Test = (): JSX.Element => {
   return (
     <div>
       test
+      <br />
       <img src="./logo.png" width={150} height={150} />
+      <br />
+      {process.env.BASE_URL}
     </div>
   );
 };
